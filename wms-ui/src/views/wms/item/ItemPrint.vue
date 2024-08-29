5c1ef82ae632f653fc2c3c27efa910d846bd0028
武锦程 <1343066628@qq.com>
2024-08-29 13:36:10 +0400

    1

--- wms-ui/src/views/wms/item/ItemPrint.vue
+++ wms-ui/src/views/wms/item/ItemPrint.vue
@@ -1,5 +1,5 @@
 <template>
-   <img :src="realSrc" width="150px" height="55px" ref="itemPrintRef"  />
+   <img :src="realSrc" width="150px" height="55px" ref="itemPrintRef" hidden />
 
 </template>
 
@@ -24,7 +24,7 @@ export default {
       this.realSrc=process.env.VUE_APP_BASE_API +this.codePath;
       console.log("11",this.realSrc)
       this.$nextTick(()=>{
-        this.$print(this.$refs.itemPrintRef, {}, '10x30 mm')
+        this.$print(this.$refs.itemPrintRef, {}, '30px 10px')
       })
     },
   }
@@ -33,6 +33,7 @@ export default {
 
 <style lang="stylus" media="print">
 @page {
+ size:portrait;
   img{
     transform : rotate(90deg);
   }
